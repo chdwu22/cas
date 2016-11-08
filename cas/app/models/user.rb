7f18e225ce4901a603b5191e71ed37c9ec22294e
Chengde Wu <chdwu22@gmail.com>
2016-11-08 02:08:15 +0000

    user sign up

--- cas/app/models/user.rb
+++ cas/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 25 }
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 25 }
+  validates :full_name, uniqueness: { case_sensitive: false }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  
+  has_secure_password
 end

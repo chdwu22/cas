26ae28fffe63473326ad577fe0e8902b611517f2
Chengde Wu <chdwu22@gmail.com>
2016-11-08 03:32:29 +0000

    login functioning

--- cas/app/controllers/sessions_controller.rb
+++ cas/app/controllers/sessions_controller.rb
@@ -2,9 +2,30 @@ class SessionsController < ApplicationController
    def new
    end
    
-   def create
-   end
-   
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user 
+      if user.authenticate(params[:session][:password])
+        session[:user_id] = user.id
+        flash[:success] = "Successfully logged in"
+        if !user.is_admin
+          redirect_to user_path(:id => user.id)
+        else
+          redirect_to users_path
+        end
+      else
+        flash.now[:notice] = "Error logging in."
+        render :new
+      end
+    else
+      flash.now[:notice] = "Error logging in."
+      render :new
+    end
+  end
+
    def destroy
+      session[:user_id] = nil
+      flash[:success] = "You have logged out"
+      redirect_to root_path
    end
 end

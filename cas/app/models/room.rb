b16ac96c79763a8273972a5308a737cba4b314c2
Chengde Wu <chdwu22@gmail.com>
2016-11-08 19:40:17 +0000

    set room and building association and validations

--- cas/app/models/room.rb
+++ cas/app/models/room.rb
@@ -1,2 +1,6 @@
 class Room < ApplicationRecord
+  belongs_to :building
+  validates :number, presence: true, numericality: { only_integer: true }
+  validates :capacity, presence: true, numericality: { only_integer: true }
+  validates :building_id, presence: true, numericality: { only_integer: true }
 end

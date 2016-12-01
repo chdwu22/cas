a1d10b864da425a86aa4303cf2911b8a60e2b481
Chengde Wu <chdwu22@gmail.com>
2016-12-01 02:17:22 +0000

    room name uniqueness validation

--- cas/app/models/room.rb
+++ cas/app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
   #belongs_to :building
   has_many :courses
-  validates :number, presence: true
+  validates :number, presence: true, uniqueness: true
   validates :capacity, presence: true, numericality: { only_integer: true }
   #validates :building_id, presence: true, numericality: { only_integer: true }
 end
